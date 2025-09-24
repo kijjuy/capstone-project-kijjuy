87ad8bdc58d27a63fe80b0c6fa763780fb1e3a0c
Brandon Zomer <brandonzomer@proton.me>
2025-09-24 12:51:31 -0400

    add props to category model

--- app/Models/Category.cs
+++ app/Models/Category.cs
@@ -4,5 +4,10 @@ namespace app.Models;
 
 public class Category
 {
+    [Key]
+    public required int CategoryId { get; set; }
 
+    [Required]
+    [MaxLength(30)]
+    public required String CategoryName { get; set; }
 }
