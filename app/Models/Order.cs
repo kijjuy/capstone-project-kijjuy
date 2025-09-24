2c80e776bb731ee2ba0872d4511aa2d8fa28ff2a
Brandon Zomer <brandonzomer@proton.me>
2025-09-24 14:08:18 -0400

    add props to order model

--- app/Models/Order.cs
+++ app/Models/Order.cs
@@ -5,4 +5,25 @@ namespace app.Models;
 public class Order
 {
 
+    [Key]
+    public required int OrderId { get; set; }
+
+    //Add UserId when auth added
+
+    public required decimal TaxPaid { get; set; }
+
+    public required decimal ShippingPaid { get; set; }
+
+    public required decimal TotalPaid { get; set; }
+
+    [MaxLength(100)]
+    public required String ShippingAddress { get; set; }
+
+    [MaxLength(80)]
+    public String? ShippingName { get; set; }
+
+    [Length(4, 4)]
+    public String? CreditCardLastFourDigits { get; set; }
+
+    public required DateTime OrderDate { get; set; }
 }
