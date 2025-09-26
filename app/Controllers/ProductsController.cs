42bd0b4428c3f2a0033bc3e3e33f4169ac1581c1
Brandon Zomer <brandonzomer@proton.me>
2025-09-25 21:34:04 -0400

    add repository dependency, add product endpoint

--- app/Controllers/ProductsController.cs
+++ app/Controllers/ProductsController.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using app.Repositories;
 
 namespace app.Controllers;
 
 public class ProductsController : ControllerBase
 {
     private readonly ILogger<ProductsController> _logger;
+    private readonly IProductsRepository _product;
 
-    public ProductsController(ILogger<ProductsController> logger)
+    public ProductsController(ILogger<ProductsController> logger,
+        IProductsRepository productsRepository)
     {
         _logger = logger;
+        _product = productsRepository;
     }
 
     [HttpGet("hello")]
@@ -16,6 +20,14 @@ public class ProductsController : ControllerBase
     {
         _logger.LogDebug("Hit Hello World method");
         return Ok("Hello\n");
+    }
+
+    [HttpGet("products")]
+    public IActionResult GetAllProducts()
+    {
+        _logger.LogDebug("Hit GetAllProducts method.");
+        _product.GetAllProducts();
+        return Ok("Product go here\n");
     }
 }
 
