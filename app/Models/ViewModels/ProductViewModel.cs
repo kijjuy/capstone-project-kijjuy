84d7ef88f75ec3964b2384ce90bd2bf3533e51ab
Brandon Zomer <brandonzomer@proton.me>
2025-09-26 19:32:42 -0400

    add products view model

---
+++ app/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,18 @@
+
+
+/**
+ * View model class for product containing only
+ * info nessecary for frontend
+ */
+public class ProductViewModel
+{
+    public required long ProductId { get; set; }
+
+    public required String CategoryName { get; set; }
+
+    public required String ProductName { get; set; }
+
+    public required double Price { get; set; }
+
+    public required String Description { get; set; }
+}
