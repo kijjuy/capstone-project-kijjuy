f2c6b7c823f463204fc5590bf5b028dba9e54cda
Brandon Zomer <brandonzomer@proton.me>
2025-09-26 16:27:23 -0400

    added fetching products to products endpoint

--- app/Controllers/ProductsController.cs
+++ app/Controllers/ProductsController.cs
@@ -6,13 +6,13 @@ namespace app.Controllers;
 public class ProductsController : ControllerBase
 {
     private readonly ILogger<ProductsController> _logger;
-    private readonly IProductsRepository _product;
+    private readonly IProductsRepository _products;
 
     public ProductsController(ILogger<ProductsController> logger,
         IProductsRepository productsRepository)
     {
         _logger = logger;
-        _product = productsRepository;
+        _products = productsRepository;
     }
 
     [HttpGet("hello")]
@@ -26,7 +26,16 @@ public class ProductsController : ControllerBase
     public IActionResult GetAllProducts()
     {
         _logger.LogDebug("Hit GetAllProducts method.");
-        _product.GetAllProducts();
+        var products = _products.GetAllProducts();
+        _logger.LogDebug($"size of products: {products.Count}");
+        foreach (var product in products)
+        {
+            var props = product.GetType().GetProperties();
+            foreach (var prop in props)
+            {
+		var val = prop.GetValue()
+            }
+        }
         return Ok("Product go here\n");
     }
 }
