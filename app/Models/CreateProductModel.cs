6cbcefeb6eb17dcc9130f5c3f4ef86b28af7e25d
Brandon Zomer <brandonzomer@proton.me>
2025-10-12 11:24:50 -0400

    add CreateProductModel :

---
+++ app/Models/CreateProductModel.cs
@@ -0,0 +1,10 @@
+public class CreateProductModel
+{
+    public required int CategoryId { get; set; }
+
+    public required String Name { get; set; }
+
+    public required decimal Price { get; set; }
+
+    public required String Description { get; set; }
+}
