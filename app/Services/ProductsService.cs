d8ddd72ef43f24015abd3fd57f96fd826d5a3c03
Brandon Zomer <brandonzomer@proton.me>
2025-10-12 11:23:56 -0400

    add delete method, create method, refactor get all method

--- app/Services/ProductsService.cs
+++ app/Services/ProductsService.cs
@@ -3,42 +3,87 @@ using app.Models;
 
 namespace app.Services;
 
-public interface IProductsService {
+public interface IProductsService
+{
     public List<ProductViewModel> GetAllProducts();
+    public void DeleteProduct(int productId);
+    public int CreateProduct(CreateProductModel product);
 }
 
 /**
  */
-public class ProductsService : IProductsService {
+public class ProductsService : IProductsService
+{
 
     private readonly ILogger<ProductsService> _logger;
     private readonly IProductsRepository _products;
 
     public ProductsService(ILogger<ProductsService> logger,
-	    IProductsRepository products) {
-	_logger = logger;
-	_products = products;
+        IProductsRepository products)
+    {
+        _logger = logger;
+        _products = products;
     }
 
     /**
      * Gets all products from the database and converts 
      * them into ProductViewModels.
      */
-    public List<ProductViewModel> GetAllProducts() {
-	List<ProductDataModel> dataProducts = _products.GetAllProducts();
-	List<ProductViewModel> viewProducts = new List<ProductViewModel>();
+    public List<ProductViewModel> GetAllProducts()
+    {
+        List<ProductDataModel> dataProducts = _products.GetAllProducts();
+        List<ProductViewModel> viewProducts = new List<ProductViewModel>();
 
-	foreach(var dataProduct in dataProducts) {
-	    //TODO: Get categories here from category repository
-	    var viewProduct = new ProductViewModel{
-		ProductId = dataProduct.ProductId,
-		CategoryName = $"Category {dataProduct.CategoryId}",
-		ProductName = dataProduct.Name,
-		Price = dataProduct.Price,
-		Description = dataProduct.Description,
-	    };
-	    viewProducts.Add(viewProduct);
-	}
-	return viewProducts;
+        foreach (var dataProduct in dataProducts)
+        {
+            //TODO: Get categories here from category repository
+            var viewProduct = new ProductViewModel
+            {
+                ProductId = dataProduct.ProductId,
+                CategoryName = $"Category {dataProduct.CategoryId}",
+                ProductName = dataProduct.Name,
+                Price = dataProduct.Price,
+                Description = dataProduct.Description,
+            };
+            viewProducts.Add(viewProduct);
+        }
+        return viewProducts;
     }
+
+    public void DeleteProduct(int productId)
+    {
+        int result = _products.DeleteProduct(productId);
+        if (result != 1)
+        {
+            _logger.LogError($"Error deleting product with id={productId}. Expected to delete 1 row, instead deleted {result}.");
+            throw new BadSqlResultException($"Error deleting product with id={productId}. Expected to delete 1 row, instead deleted {result}.");
+        }
+    }
+
+    public int CreateProduct(CreateProductModel product)
+    {
+        if (product.Name == null ||
+            product.Price == 0 ||
+            product.CategoryId == 0 ||
+            product.Description == null)
+        {
+            _logger.LogWarning($"Attempted to create product with null values.");
+            throw new ArgumentException("Cannot create product with null values.");
+        }
+
+        int result = _products.CreateProduct(product);
+        if (result < 1)
+        {
+            _logger.LogError($"Error inserting new product. Expected newId >= 1, got newId={result}.");
+            throw new BadSqlResultException($"Error inserting new product. Expected newId >= 1, got newId={result}.");
+        }
+        return result;
+    }
+}
+
+public class BadSqlResultException : Exception
+{
+    public BadSqlResultException(String message)
+    : base(message)
+    { }
 }
