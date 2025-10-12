174695a4713548fe687929f02631e7fc19f9dce8
Brandon Zomer <brandonzomer@proton.me>
2025-10-12 14:19:55 -0400

    init DeleteProductTests

---
+++ tests/Services/ProductsService/DeleteProductTests.cs
@@ -0,0 +1,64 @@
+using app.Services;
+using app.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace tests;
+
+public class DeleteProductTests
+{
+    private readonly ILogger<ProductsService> _serviceLogger;
+
+    public DeleteProductTests()
+    {
+        var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+        _serviceLogger = loggerFactory.CreateLogger<ProductsService>();
+    }
+
+    //To test: 
+    //Delete with valid id and product exists returns 1
+    //Delete with valid id and products doesn't exist (return 0 from repo) throws BadSqlDataException
+    //Delete with invalid id (0) throws ArgumentException
+
+
+    #region DeleteProduct
+
+    /**
+     * <summary>
+     * Tests that service getting a valid response from the repo
+     * does not throw any exception.
+     * </summary>
+     */
+    [Fact]
+    public void ProductIsDeletedDoesNothing()
+    {
+        //arrange
+        var mockRepo = new Mock<IProductsRepository>();
+        mockRepo.Setup(repo => repo.DeleteProduct(1))
+            .Returns(1);
+
+        var service = new ProductsService(_serviceLogger, mockRepo.Object);
+
+        //act
+        //assert
+        service.DeleteProduct(1);
+    }
+
+    [Fact]
+    public void InvalidProductIdThrowsArgumentException()
+    {
+        //arrange
+        var mockRepo = new Mock<IProductsRepository>();
+        var service = new ProductsService(_serviceLogger, mockRepo.Object);
+
+        //act
+        //assert
+        Assert.Throws<ArgumentException>(() => service.DeleteProduct(0));
+    }
+
+    #endregion
+
+}
