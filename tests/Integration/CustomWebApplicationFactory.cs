23bd09ce636f434b0797901c90ea76bbb4975cd8
Brandon Zomer <brandonzomer@proton.me>
2025-10-19 17:12:58 -0400

    init CustomWebApplicationFactory

---
+++ tests/Integration/CustomWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration((context, configBuilder) =>
+        {
+            var testSettings = new Dictionary<String, String?>();
+            testSettings["ConnectionStrings:DefaultConnection"] = "Data Source=test.db";
+
+            configBuilder.AddInMemoryCollection(testSettings);
+        });
+    }
+}
