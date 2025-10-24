aeae9f3a3251734181594c30146fed31f5a76546
Brandon Zomer <brandonzomer@proton.me>
2025-10-24 19:22:55 -0400

    add product details tests

--- tests/Integration/Products/ProductDetailsTests.cs
+++ tests/Integration/Products/ProductDetailsTests.cs
@@ -1,4 +1,5 @@
 using app;
+using System.Net;
 
 namespace tests;
 
@@ -27,5 +28,35 @@ public class ProductDetailsTests : IClassFixture<CustomWebApplicationFactory<Pro
         var body = await response.Content.ReadAsStringAsync();
 
         Assert.Contains(product.Name, body);
+    }
+
+    [Fact]
+    public async Task ProductNotExists_ReturnsNotFound()
+    {
+	//arrange
+	var factory = new CustomWebApplicationFactory<Program>();
+	var client = factory.CreateDefaultClient();
+	DbHelper.initDb(factory.connectionString);
+
+	//act
+	var response = await client.GetAsync("/products/1");
+
+	//assert
+	Assert.Equal(response.StatusCode, HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task InvalidProductId_ReturnsBadRequest()
+    {
+	//arrange
+	var factory = new CustomWebApplicationFactory<Program>();
+	var client = factory.CreateDefaultClient();
+	DbHelper.initDb(factory.connectionString);
+
+	//act
+	var response = await client.GetAsync("/products/0");
+
+	//
+	Assert.Equal(response.StatusCode, HttpStatusCode.BadRequest);
     }
 }
