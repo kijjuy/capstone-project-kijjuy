03874b598e6a6367cd583bcf79f72e830b18fbda
Brandon Zomer <brandonzomer@proton.me>
2025-10-24 19:26:40 -0400

    add identity migrations

---
+++ app/Migrations/20251024145753_ChangeCartFromProductListToLongList.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace app.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCartFromProductListToLongList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cart",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cart",
+                table: "AspNetUsers");
+        }
+    }
+}
