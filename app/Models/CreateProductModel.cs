818360cdb1caf0112ac64e5fda9a825d470c5dcd
Brandon Zomer <brandonzomer@proton.me>
2025-10-26 12:32:45 -0400

    add data annotation boundry tags

--- app/Models/CreateProductModel.cs
+++ app/Models/CreateProductModel.cs
@@ -1,12 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace app.Models;
 
 public class CreateProductModel
 {
+    [Range(1, 20)]
     public required int CategoryId { get; set; }
 
+    [Length(minimumLength: 3, maximumLength: 100)]
     public required String Name { get; set; }
 
+    [Range(1, 1000)]
     public required decimal Price { get; set; }
 
+    [Length(minimumLength: 10, maximumLength: 1000)]
     public required String Description { get; set; }
 }
