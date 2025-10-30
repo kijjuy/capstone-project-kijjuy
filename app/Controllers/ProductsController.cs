d4317c9a1a5b252834182e8e1052b9c2f70bc70d
Brandon Zomer <brandonzomer@proton.me>
2025-10-30 11:50:48 -0400

    updated todo

--- app/Controllers/ProductsController.cs
+++ app/Controllers/ProductsController.cs
@@ -72,7 +72,7 @@ public class ProductsController : Controller
     [HttpGet("/images/{imageName}")]
     public async Task<IActionResult> GetImageByName(String imageName)
     {
-	//Change to image service
+	//TODO: Change to image service, move this code to new images controller
 	var filePath = AppContext.BaseDirectory + "Images/" + imageName;
 	var fileContent = await System.IO.File.ReadAllBytesAsync(filePath);
 	return File(fileContent, "image/jpeg");
