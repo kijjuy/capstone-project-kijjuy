7867db42fda48aa7a1f350973e10728bc58245b8
Brandon Zomer <brandonzomer@proton.me>
2025-11-10 14:48:20 -0500

    remove unused methods from ProductsService

--- app/Services/ProductsService.cs
+++ app/Services/ProductsService.cs
@@ -12,10 +12,6 @@ public interface IProductsService
     public void DeleteProduct(int productId);
     public Task<int> CreateProduct(CreateProductModel product);
     public Task UpdateProduct(UpdateProductModel product, int id);
-    public Task<List<ProductViewModelWithImages>> GetProductsFromCart(List<long> cart);
-    public Task<CartViewModel> GetCartViewModelFromCart(List<long> cart);
-    public Task<bool> AddProductToCart(List<long> cart, int productId);
-    public bool RemoveFromCart(List<long> cart, int productId);
 }
 
 public class ProductsService : IProductsService
@@ -263,83 +259,6 @@ public class ProductsService : IProductsService
         return imageName;
     }
 
-    /**
-     * <summary>
-     * Loops through product ids in <paramref name="cart"/> and gets each product, adding them to
-     * a list. Returns the list of those products.
-     * </summary>
-     */
-    public async Task<List<ProductViewModelWithImages>> GetProductsFromCart(List<long> cart)
-    {
-        var products = new List<ProductViewModelWithImages>();
-        foreach (int productId in cart)
-        {
-            var product = await GetProductByIdWithImages(productId);
-            if (product == null)
-            {
-                _logger.LogWarning($"Cart item with productId={productId} was not found. Now removing it from the cart.");
-                cart.Remove(productId);
-                continue;
-            }
-            products.Add(product);
-        }
-        return products;
-    }
-
-    /**
-     * <summary>
-     * Gets view models from cart, then gets the sum of their prices and creates a new CartViewModel 
-     * out of view models list and sum of prices for subtotal.
-     * </summary>
-     */
-    public async Task<CartViewModel> GetCartViewModelFromCart(List<long> cart)
-    {
-        var products = await GetProductsFromCart(cart);
-        double subtotal = 0;
-        foreach (var product in products)
-        {
-            subtotal += product.InternalModel.Price;
-        }
-
-        return new CartViewModel
-        {
-            Products = products,
-            Subtotal = subtotal
-        };
-    }
-
-    /**
-     * <summary>
-     * Takes in <paramref name="cart"/>, checks if the product exists, then adds it to <paramref name="cart"/>.
-     * <see langword="return"/> <see langword="true"/> if product is added. 
-     * <see langword="return"/> <see langword="false"/> if product was not added.
-     * </summary>
-     */
-    public async Task<bool> AddProductToCart(List<long> cart, int productId)
-    {
-        _logger.LogDebug($"Count of cart before adding: ${cart.Count()}");
-        var product = GetProductById(productId);
-        if (product == null)
-        {
-            _logger.LogWarning($"Cound not find product with id={productId}. Returning same cart.");
-            return false;
-        }
-
-        cart.Add(productId);
-        _logger.LogDebug($"Count of cart after adding: ${cart.Count()}");
-        return true;
-    }
-
-    /**
-     * <summary>
-     * Checks if <paramref name="productId"/> is inside of <paramref name="cart"/>. If not, <see langword="return"/>
-     * <see langword="false"/>. If yes, <see langword="return"/> <see langword="true"/>
-     * </summary>
-     */
-    public bool RemoveFromCart(List<long> cart, int productId)
-    {
-        return cart.Remove(productId);
-    }
 
 
     /**
