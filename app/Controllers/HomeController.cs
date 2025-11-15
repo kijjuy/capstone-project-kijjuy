dd86c7b475e09167057ad68e2a679d8426423064
Brandon Zomer <brandonzomer@proton.me>
2025-11-14 22:16:15 -0500

    add getting category names to home index page

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -1,14 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using app.Services;
 
 namespace app.Controllers;
 
 public class HomeController : Controller
 {
     protected readonly ILogger<HomeController> _logger;
+    private readonly ICategoriesService _categoriesService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        ICategoriesService categoriesService
+        )
     {
         _logger = logger;
+        _categoriesService = categoriesService;
     }
 
     /**
@@ -17,8 +23,32 @@ public class HomeController : Controller
      * </summary>
      */
     [HttpGet("/")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var categories = await _categoriesService.GetAllCategories();
+
+        foreach (var category in categories)
+        {
+            _logger.LogDebug($"cat id: {category.CategoryId} ; cat name: {category.CategoryName}");
+        }
+
+        var cuttingBoard = categories
+            .Where(c => c.CategoryName.Equals("Cutting Boards"))
+            .FirstOrDefault();
+
+        var charcuterie = categories
+            .Where(c => c.CategoryName.Equals("Charcuterie Boards"))
+            .FirstOrDefault();
+
+        var choppingBlock = categories
+            .Where(c => c.CategoryName.Equals("Chopping Blocks"))
+            .FirstOrDefault();
+
+
+        ViewData["cuttingBoard"] = cuttingBoard == null ? "" : cuttingBoard.CategoryName;
+        ViewData["charcuterie"] = charcuterie == null ? "" : charcuterie.CategoryName;
+        ViewData["choppingBlock"] = choppingBlock == null ? "" : choppingBlock.CategoryName;
+
         return View();
     }
 }
