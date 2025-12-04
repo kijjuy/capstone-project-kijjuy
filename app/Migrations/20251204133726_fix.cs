17b7bd52c3cf862ac306567a3bf1b158139e1b0c
Brandon Zomer <brandonzomer@proton.me>
2025-12-04 13:36:59 -0500

    add migrations

---
+++ app/Migrations/20251204133726_fix.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace app.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentOrderId",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentOrderId",
+                table: "AspNetUsers");
+        }
+    }
+}
